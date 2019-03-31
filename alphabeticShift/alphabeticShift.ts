df62cef350771427cdaeb964585bc1f83ce67d09
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 14:27:00 +1000

    solve 'alphabetic shift ' task

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,14 @@
 function alphabeticShift(inputString: string): string {
+    let result = '';
+    for (let char of inputString) {
+        let code = char.charCodeAt(0);
+        code = (char === 'z') ? 'a'.charCodeAt(0) - 1 : code;
+        code = (char === 'Z') ? 'A'.charCodeAt(0) - 1 : code;
 
+        result += String.fromCharCode(code + 1);
+    }
+
+    return result;
 }
 
 console.log(alphabeticShift('crazy'));
