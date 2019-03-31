61aca1a2900795ba148ff764374a9b65a228d686
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 18:39:42 +1000

    solve 'alternating sums' task

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 function alternatingSums(a: number[]): number[] {
+    let evenSum = 0;
+    let oddSum = 0;
 
+    a.forEach((elem, index) => {
+        if (index % 2 == 0) {
+            evenSum += elem;
+        } else {
+            oddSum += elem;
+        }
+    });
+
+    return [evenSum, oddSum];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
