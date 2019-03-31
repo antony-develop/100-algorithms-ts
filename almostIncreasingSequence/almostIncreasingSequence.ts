be0237eee5696595efba485f8e2ae848b3bcf0b0
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 13:57:54 +1000

    solve 'almost increasing sequence' task

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,14 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+    let count = 0;
+    for (let i = 0; i < sequence.length - 1; i++) {
+        if (sequence[i] >= sequence[i+1]) {
+            count++;
+        }
+    }
 
+    return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
+console.log(almostIncreasingSequence([1, 3]));
