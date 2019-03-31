a87c9aeb04598a07c5f58bd1e50a6697a347b695
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 19:29:03 +1000

    solve 'are similar' task

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,21 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   
+    const aDiff = [];
+    let bDiff = [];
+    for (let i = 0; i < a.length; i++) {
+       if (a[i] != b[i]) {
+           aDiff.push(a[i]);
+           bDiff.push(b[i]);
+       }
+    }
+    
+    if (aDiff.length <= 2) {
+        bDiff = bDiff.reverse();
+        return (aDiff.toString() == bDiff.toString());
+    }
+
+    return false;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 3, 3], [2, 1, 3, 3]));
 console.log(areSimilar([1, 2, 2], [2, 1, 1]));
