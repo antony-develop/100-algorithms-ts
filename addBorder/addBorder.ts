1b3c2f859be63d41dd61192238318fd932d42809
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 11:07:32 +1000

    solve "add border" task

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,14 @@
 function addBorder(picture: string[]): string[] {
+    const horizontalBorder = '*'.repeat(picture[0].length + 2);
 
+    picture = picture.map(item => {
+        return `*${item}*`;
+    });
+
+    picture.unshift(horizontalBorder);
+    picture.push(horizontalBorder);
+
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
