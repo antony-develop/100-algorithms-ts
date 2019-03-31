791e0a9f8bbe15a238e3eeaea40dea13e7ba0909
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 12:18:29 +1000

    solve "largest adjacent elements product"

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let max = inputArray[0] * inputArray[1];
 
+    for (let i = 1; i < inputArray.length - 1; i++) {
+        let product = inputArray[i] * inputArray[i+1];
+        max = product > max ? product : max;
+    }
+
+    return max;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
