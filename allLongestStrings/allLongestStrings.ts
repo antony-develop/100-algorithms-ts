19c5eca50820e5539ab0128c6d8f9046fb67905d
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-31 13:28:08 +1000

    solve "get all longest strings" task

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    let maxLenght = 0;
+    let longestArray: string[] = [];
 
+    for (let str of inputArray) {
+        if (str.length > maxLenght) {
+            maxLenght = str.length;
+            longestArray = [];
+        }
+
+        if (str.length == maxLenght) {
+            longestArray.push(str);
+        }
+    }
+
+    return longestArray;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["a", "aba", "aa", "ad", "vcd", "aba"]));
