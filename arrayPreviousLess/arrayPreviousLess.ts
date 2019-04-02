d659aeabdc19fdec2e12745a06991e71a0aa94ed
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-03 08:36:02 +1000

    solve 'array previous less' task

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,19 @@
 function arrayPreviousLess(items: number[]): number[] {
+    const result = [];
+    for (let i = 0; i < items.length; i++) {
+        for (let j = i; j >= 0; j--) {
+            if (items[j] < items[i]) {
+                result.push(items[j]);
+                break;
+            }
 
+            if (j == 0) {
+                result.push(-1);
+            }
+        }
+    }
+
+    return result;
 }
 
-console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 9, 5, 2, 4, 5]));
