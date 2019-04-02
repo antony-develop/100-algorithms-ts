e6a8af0e51ab58ce32cdecfa7de6d53fdfdbbd67
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-03 06:47:10 +1000

    solve 'max consecutive sum' task

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,11 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    let sums = [];
+    for (let i = 0; i <= inputArray.length - k; i++) {
+        let sum = inputArray.slice(i, i + k).reduce((a, b) => a + b);
+        sums.push(sum);
+    }
 
+    return Math.max(...sums);
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
