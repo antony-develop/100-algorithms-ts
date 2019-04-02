2f2785e3b1932353179f1aa24b9f2fa81ddd9649
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-03 08:44:38 +1000

    solve 'array replace' task

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,11 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    inputArray.forEach((elem, i) => {
+        if (elem === elemToReplace) {
+            inputArray[i] = substitutionElem;
+        }
+    });
+
+    return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
