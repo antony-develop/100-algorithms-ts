94d094a6f7da39328f4f234d47933ca7ae723094
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-03 07:23:59 +1000

    solve 'maximal adjacent difference' task

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,11 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let max = 0;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        let diff = Math.abs(inputArray[i] - inputArray[i+1]);
+        max = (diff > max) ? diff : max;
+    }
 
+    return max;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
