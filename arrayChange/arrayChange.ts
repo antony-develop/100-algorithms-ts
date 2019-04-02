f81393e3c06f587acc9936f8118d77bc03f315a5
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-02 11:10:03 +1000

    solve 'array change' task

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,15 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let moves = 0;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        if (inputArray[i] >= inputArray[i+1]) {
+            moves += inputArray[i] - inputArray[i+1] + 1;
+            inputArray[i+1] = inputArray[i] + 1;
+        }
+    }
+
+    return moves;
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([3, 1, 1]));
+console.log(arrayChange([7, 2, 4]));
