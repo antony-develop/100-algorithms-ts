38155972e5af8a57f76dc5c1ab3b7f6f125eecff
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-05 20:52:57 +1000

    solve 'box blur' task

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,72 @@
 function boxBlur(image: number[][]): number[][] {
+    const neighbour = 1;
+    const result = [];
+    for (let y = 1; y < image.length - neighbour; y++) {
+        let line = [];
+        for (let x = 1; x < image[y].length - neighbour; x++) {
+            line.push(pixelAvarage(image, x, y, neighbour));
+        }
+        result.push(line);
+    }
 
+    return result;
 }
 
-console.log(boxBlur([[1, 1, 1], 
-    [1, 7, 1], 
-    [1, 1, 1]]));
+function pixelAvarage(image, pixelX, pixelY, neighbour) {
+    let sum = 0;
+    let count = 0;
+
+    for (let y = pixelY - neighbour; y <= pixelY + neighbour; y++) {
+        for (let x = pixelX - neighbour; x <= pixelX + neighbour; x++) {
+            sum += image[y][x];
+            count++;
+        }
+    }
+
+    return Math.floor(sum / count);
+}
+
+function boxBlur2(image: number[][]): number[][] {
+    const res = [];
+
+    for (let y = 0; y < image.length - 2; y++) {
+        const line = [];
+        
+        for (let x = 0; x < image[y].length - 2; x++) {
+            let sum = 0;
+            let count = 0;
+
+            for (let a = y; a < y + 3; a++) {
+                for (let b = x; b < x + 3; b++) {
+                    sum += image[a][b];
+                    count++;
+                }
+            }
+
+            line.push(Math.floor(sum/count));
+        }
+
+        res.push(line);
+    }
+
+    return res;
+}
+
+console.log(boxBlur([
+    [1, 1, 1],
+    [1, 7, 1],
+    [1, 1, 1]
+]));
+
+console.log(boxBlur([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0,90,90,90,90,90, 0, 0],
+    [0, 0, 0,90, 0,90,90,90, 0, 0],
+    [0, 0, 0,90,90,90,90,90, 0, 0],
+    [0, 0, 0,90,90,90,90,90, 0, 0],
+    [0, 0, 0,90,90,90,90,90, 0, 0],
+    [0,90, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]));
