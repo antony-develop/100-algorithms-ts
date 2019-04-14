b2d6e7016b27b67a560d3c1436c60dcb5d252394
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-14 19:59:08 +1000

    solve 'character parity' task

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,5 +1,11 @@
 function characterParity(symbol: string): string {
-   
+   const input = Number(symbol);
+
+   if (isNaN(input)) {
+       return 'not a digit';
+   }
+
+   return input % 2 === 0 ? 'even' : 'odd';
 }
 
 console.log(characterParity('5'))
