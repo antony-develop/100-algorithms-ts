43def28a7cfff9cc027ce8d19bf7e3ddd84fe4b3
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-14 19:06:28 +1000

    solve 'case insensitive palindrome' task

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,19 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
+    inputString = inputString.toLowerCase();
 
+    const sideLength = Math.floor(inputString.length / 2);
+
+    for (let i = 0; i < sideLength; i++) {
+        if (inputString[i] != inputString[inputString.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('abbc'));
+console.log(isCaseInsensitivePalindrome('aCdcA'));
+console.log(isCaseInsensitivePalindrome('asdfgggfdsa'));
