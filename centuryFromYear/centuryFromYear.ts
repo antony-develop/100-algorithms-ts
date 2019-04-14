6dfe36e2686bf00ea8800e2dc5bddca0ca918ecc
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-14 19:45:13 +1000

    solve 'century from year' task

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,12 @@
 function centuryFromYear(year: number): number {
- 
+    if (year % 100 === 0) {
+        return year / 100;
+    }
+
+    return Math.floor(year / 100) + 1;
 }
 
 console.log(centuryFromYear(1905));
 console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1701));
+console.log(centuryFromYear(1));
