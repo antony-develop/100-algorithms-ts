69816efa8cf2d70ef5a2bda20e5c87a22107e1fe
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 00:36:56 +1000

    solve 'common character count' task

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,26 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+    const charCount1 = countCharacters(s1);
+    const charCount2 = countCharacters(s2);
+    let total = 0;
+
+    for (let char in charCount1) {
+        const count1 = charCount1[char];
+        const count2 = charCount2[char];
+        if (count2) {
+            total += Math.min(count1, count2);
+        }
+    }
+
+    return total;
+}
+
+function countCharacters(str) {
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+    }
+
+    return charCount;
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
