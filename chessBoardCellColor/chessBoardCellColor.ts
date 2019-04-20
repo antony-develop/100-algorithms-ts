08f6e4f6597b25b60676ede8cefa0fea91003fc8
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-20 20:39:00 +1000

    solve 'chess board cell color' task

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,6 +1,24 @@
-function chessBoardCellColor(cell1: string, cell2: string): boolean {
+interface Cell {
+    x: number,
+    y: number
+}
 
+function chessBoardCellColor(cellStr1: string, cellStr2: string): boolean {
+    const cell1 = parseCell(cellStr1);
+    const cell2 = parseCell(cellStr2);
+
+    return (cell1.x + cell1.y) % 2 === (cell2.x + cell2.y) % 2;
+}
+
+function parseCell(cellStr: String): Cell {
+    cellStr = cellStr.toLowerCase();
+
+    return {
+        x: cellStr.charCodeAt(0),
+        y: Number(cellStr[1])
+    }
 }
 
 console.log(chessBoardCellColor('A1', 'C3'));
 console.log(chessBoardCellColor('A1', 'H3'));
+console.log(chessBoardCellColor('G7', 'H8'));
