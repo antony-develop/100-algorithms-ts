157bd39614d6fb4b31e8a98f5661e16e9b1ae467
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-20 19:54:44 +1000

    solve 'check palindrome' task

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,5 +1,12 @@
 function checkPalindrome(inputString: string): boolean {
+    const sideLength = Math.floor(inputString.length / 2);
+    for (let i = 0; i < sideLength; i++) {
+        if (inputString[i] !== inputString[inputString.length - 1 - i]) {
+            return false;
+        }
+    }
 
+    return true;
 }
 
 console.log(checkPalindrome('aabaa'));
