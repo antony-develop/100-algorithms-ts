e2127df3e7435c0a50b9520dd63b2169244311d4
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 00:02:17 +1000

    solve 'circle of numbers' task

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,8 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
-
+    let position = n / 2 + firstNumber;
+    position = position > n ? position - n : position;
+    return position;
 }
 
 console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 6));
