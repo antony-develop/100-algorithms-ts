cb0b7cb0794fa14f5d2854a5189480381af2f131
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 15:24:19 +1000

    solve contains duplicates' task

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,14 @@
 function containsDuplicates(a: number[]): boolean {
+    const valuesCount = {};
+    for (let num of a) {
+        if (valuesCount[num]) {
+            return true;
+        }
 
+        valuesCount[num] = 1;
+    }
+
+    return false;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
