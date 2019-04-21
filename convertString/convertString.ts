a8d9cdb770f97d9e3eae435e369380ef15f3cd62
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 15:49:58 +1000

    solve 'convert string' task

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,5 +1,19 @@
 function convertString(s: string, t: string): boolean {
+    let poisition = 0;
+    let word = '';
+    for (let char of t) {
+        poisition = s.indexOf(char, poisition);
+        if (poisition === -1) {
+            return false;
+        }
 
+        word += s[poisition];
+        if (word === t) {
+            return true;poisition
+        }
+    }
+
+    return true;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
