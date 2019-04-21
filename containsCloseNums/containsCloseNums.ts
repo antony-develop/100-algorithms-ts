fc0fcde5f64988234643a2cffbf736b5fba0c913
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 14:57:51 +1000

    solve 'contains close nums' task

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,5 +1,15 @@
 function containsCloseNums(nums: number[], k: number): boolean {
+    const positions = {};
 
+    for (let i = 0; i < nums.length; i++) {
+        if (positions[nums[i]] && Math.abs(positions[nums[i]] - i) <= k) {
+            return true;
+        }
+
+        positions[nums[i]] = i;
+    }
+
+    return false;
 }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
