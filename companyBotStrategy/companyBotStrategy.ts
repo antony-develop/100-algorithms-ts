909faeed085eb608904747c06fbcd444b44a522e
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 12:57:46 +1000

    solve 'company bot strategy' task

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,5 +1,17 @@
 function companyBotStrategy(trainingData: number[][]): number {
- 
+    let totalTime = 0;
+    let correctAnswers = 0;
+    for (let trainerData of trainingData) {
+        const trainerAnswerTime = trainerData[0];
+        const trainerAnswer = trainerData[1];
+
+        if (trainerAnswer === 1) {
+            totalTime += trainerAnswerTime;
+            correctAnswers++;
+        }
+    }
+
+    return correctAnswers ? totalTime / correctAnswers : 0;
 }
 
 console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
