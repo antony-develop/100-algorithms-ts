f81e556cf2b60c97b1b98eb2f451e5d2ae4d3750
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 16:08:12 +1000

    solve 'crossing sum' task

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,13 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+    let sum = 0;
+
+    matrix.forEach((row, i) => {
+        sum += (i == a) ? row.reduce((a,b) => a+b) : row[b];
+    });
+
+    return sum;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1],
+                                [2, 2, 2, 2],
+                                [3, 3, 3, 3]], 1, 3));
