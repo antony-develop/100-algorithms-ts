a0d7782e556f3850ebf86b8ddd380c6c284f1143
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-04-21 14:13:30 +1000

    solve 'composer ranges' task

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,5 +1,16 @@
 function composeRanges(nums: number[]): string[] {
+    const ranges = [];
+    let firstNum = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] + 1 !== nums[i+1] || nums.length - 1 == i) {
+            const range = (firstNum == nums[i]) ? String(firstNum) : `${firstNum}->${nums[i]}`;
+            ranges.push(range);
 
+            firstNum = nums[i+1];
+        }
+    }
+
+    return ranges;
 }
 
 
