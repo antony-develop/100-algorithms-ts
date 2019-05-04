0bc8579f48720580f2b1c7fbb92354b3ecfa3bc4
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-04 12:39:38 +1000

    solve 'deposit profit' task

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,12 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+    let compound = deposit;
+    let years = 0;
+    while (compound < threshold) {
+        compound += compound / 100 * rate;
+        years ++;
+    }
+
+    return years;
 }
 
 console.log(depositProfit(100, 20, 170))
