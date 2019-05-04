6f9f6ea3360da5fb5aae86fe326a169d245f3960
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-04 13:16:59 +1000

    solve 'digit degree' task

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,5 +1,16 @@
 function digitDegree(n: number): number {
-  
+    let number = n;
+    let degree = 0;
+    while (number >= 10) {
+        const strNums = number.toString();
+        number = 0;
+        for (let strNum of strNums) {
+            number += Number(strNum);
+        }
+        degree++
+    }
+
+    return degree;
 }
 
 console.log(digitDegree(5));
