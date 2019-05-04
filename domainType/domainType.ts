941dc54d24368325be3adaf9fb7ffb691716dd19
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-04 15:23:59 +1000

    solve 'domain type' task

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,23 @@
 function domainType(domains: string[]): string[] {
+    const descriptions = [];
+    for (let domain of domains) {
+        const domainParts = domain.split('.');
+        const topLevelDescription = getTopLevelDescription(domainParts[domainParts.length-1]);
+        descriptions.push(topLevelDescription);
+    }
 
+    return descriptions;
+}
+
+function getTopLevelDescription(topLevelDomain) {
+    const descriptions = {
+        'org': "organization",
+        'com': "commercial",
+        'net': "network",
+        'info': "information"
+    };
+
+    return descriptions[topLevelDomain];
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
