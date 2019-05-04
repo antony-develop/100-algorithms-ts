bf292e73abd8be8216c47a071c71b7f1caa1ff81
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-04 12:59:43 +1000

    solve 'different symbols naive' task

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,19 @@
-function differentSymbolsNaive(s: string): number {
- 
+function differentSymbolsNaive(string: string): number {
+    const chars = new Set();
+    for (let char of string) {
+        chars.add(char);
+    }
+
+    return chars.size;
+}
+
+function differentSymbolsNaive2(s: string): number {
+    const chars = {};
+    for (let char of s) {
+        chars[char] = char;
+    }
+
+    return Object.keys(chars).length;
 }
 
 console.log(differentSymbolsNaive('cabca'));
