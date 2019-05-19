7ac473568148f7ee4a1887a3d9d60f43b150dfac
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-19 13:48:40 +1000

    solve 'even digits only' task

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,5 +1,11 @@
 function evenDigitsOnly(n: number): boolean {
+    for (let digitStr of n.toString()) {
+        if (Number(digitStr) % 2 !== 0) {
+            return false;
+        }
+    }
 
+    return true;
 }
 
 console.log(evenDigitsOnly(248622));
