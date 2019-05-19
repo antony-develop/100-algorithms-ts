a55eeb4ce8797af36f3d3a7e295974f53e48c526
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-19 14:01:05 +1000

    solve 'extract matrix column' task

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,10 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+    const columnValues = [];
+    for (let row of matrix) {
+        columnValues.push(row[column]);
+    }
 
+    return columnValues;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
