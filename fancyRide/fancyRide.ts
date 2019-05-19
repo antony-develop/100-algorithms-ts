346b3f4166384c2ab2094c0b6174acaa8d153e43
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-19 14:47:32 +1000

    solve 'fancy ride' task

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,12 @@
 function fancyRide(l: number, fares: number[]): string {
+    const rideNames = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
 
+    for (let i = 0; i < fares.length; i++) {
+        const price = l * fares[i];
+        if (price > 20) {
+            return rideNames[i-1];
+        }
+    }
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
