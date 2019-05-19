f14395def5380db2587a777056d3d81f8f9fb1f7
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-19 13:37:54 +1000

    solve 'election winners' task

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,19 @@
 function electionsWinners(votes: number[], k: number): number {
+    const maxVote = Math.max(...votes);
 
+    if (k === 0) {
+        return maxVote;
+    }
+
+    let potentialWinners = 0;
+
+    for (let vote of votes) {
+        if (vote + k > maxVote) {
+            potentialWinners++;
+        }
+    }
+
+    return potentialWinners;
 }
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
