2a0f6417743a148cbd3a6f261d69dc3973025251
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-05-19 14:15:02 +1000

    solve 'factorialize a number' task

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,19 @@
 function factorializeANumber(num: number): number {
+    let total = 1;
 
+    for (let i = 1; i <= num; i++) {
+        total *= i;
+    }
+
+    return total;
+}
+
+function factorializeANumberRecursion(num: number): number {
+    if (num === 1) {
+        return num;
+    }
+
+    return num * factorializeANumberRecursion(num - 1);
 }
 
 console.log(factorializeANumber(5));
